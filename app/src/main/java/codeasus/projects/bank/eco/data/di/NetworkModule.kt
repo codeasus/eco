f4c73fdfa227c8ae7ca35c8e0d75fee5b4380aa9
= <=>
2025-02-26 22:01:53 +0100

    Added network client

--- app/src/main/java/codeasus/projects/bank/eco/data/di/NetworkModule.kt
+++ app/src/main/java/codeasus/projects/bank/eco/data/di/NetworkModule.kt
@@ -1,16 +1,56 @@
 package codeasus.projects.bank.eco.data.di
 
+import android.util.Log
+import codeasus.projects.bank.eco.data.di.util.ConfigurationConstants
 import codeasus.projects.bank.eco.data.remote.repository.MockPaymentRepository
 import codeasus.projects.bank.eco.domain.remote.repository.PaymentRepository
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import io.ktor.client.HttpClient
+import io.ktor.client.engine.okhttp.OkHttp
+import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.client.plugins.defaultRequest
+import io.ktor.client.plugins.logging.LogLevel
+import io.ktor.client.plugins.logging.Logger
+import io.ktor.client.plugins.logging.Logging
+import io.ktor.client.request.header
+import io.ktor.http.ContentType
+import io.ktor.http.HttpHeaders
+import io.ktor.serialization.kotlinx.json.json
+import kotlinx.serialization.json.Json
 import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 object NetworkModule {
+
+    @Provides
+    @Singleton
+    fun provideHttpClient(): HttpClient {
+        return HttpClient(OkHttp) {
+            install(Logging) {
+                logger = object : Logger {
+                    override fun log(message: String) {
+                        Log.v("Ktor", message)
+                    }
+                }
+                level = LogLevel.ALL
+            }
+            install(ContentNegotiation) {
+                json(Json {
+                    prettyPrint = true
+                    isLenient = true
+                    ignoreUnknownKeys = true
+                })
+            }
+            defaultRequest {
+                url(ConfigurationConstants.NetworkEndpoints.BASE_URL)
+                header(HttpHeaders.ContentType, ContentType.Application.Json)
+            }
+        }
+    }
 
     @Provides
     @Singleton
---
+++ app/src/main/java/codeasus/projects/bank/eco/data/di/util/ConfigurationConstants.kt
@@ -0,0 +1,7 @@
+package codeasus.projects.bank.eco.data.di.util
+
+class ConfigurationConstants {
+    object NetworkEndpoints {
+        const val BASE_URL = "https://api.example.com"
+    }
+}
